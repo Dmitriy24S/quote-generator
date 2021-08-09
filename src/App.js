512f63f5a702c5a6576cb50f3e9de0971602457f
Dmitriy <devilins24@gmail.com>
2021-08-09 20:32:14 +0300

    init project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import colors from "./colors";
 
 function App() {
+  const [color, setColor] = useState("");
+  const [author, setAuthor] = useState("");
+  const [quote, setQuote] = useState("Loading Quote");
+
+  const fetchQuote = async () => {
+    try {
+      const response = await fetch("https://type.fit/api/quotes");
+      const data = await response.json();
+      let randomNum = Math.floor(Math.random() * data.length);
+      setAuthor(data[randomNum].author);
+      setQuote(data[randomNum].text);
+    } catch (error) {
+      throw new Error(error);
+    }
+    randomizeColor();
+  };
+
+  const randomizeColor = () => {
+    let randomNum = Math.floor(Math.random() * colors.length);
+    setColor(colors[randomNum]);
+  };
+
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: color }}>
+      <div className="container">
+        <div className="card">
+          <h5 className="card-header">Random Quote Generator</h5>
+          <div className="card-body">
+            <h5 className="card-title">{author}</h5>
+            <p className="card-text">{quote}</p>
+            <button className="btn btn-primary" onClick={() => fetchQuote()}>
+              New Quote
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
